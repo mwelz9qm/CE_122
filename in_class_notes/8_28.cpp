dfd236777260d8f2fe4dd8261dd34ec9f9662f09
Matt Welz <matthew.d.welz@gmail.com>
2024-08-28 14:55:49 -0600

    added 8_28 notes

---
+++ in_class_notes/8_28.cpp
@@ -0,0 +1,30 @@
+/*
+Matt Welz
+Class Notes from 8/28/2024
+CE 122
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    //print to screen
+    cout << "Hello, world!" << endl;
+
+    //declare some integer variables
+
+    int x, y, z;
+
+    //asign the variables some values
+
+    x = 5;
+    y = 22;
+    z = x * y;
+
+    cout << "The product of x and y is: " << z << endl;
+    cout << "The value of x squared is: " << x * x << endl;
+
+    return 0;
+}
