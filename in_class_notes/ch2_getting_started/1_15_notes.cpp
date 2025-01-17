1a14e785035bdbcc559b61745a1142413f26caeb
Matt Welz <matthew.d.welz@gmail.com>
2025-01-17 08:21:30 -0700

    cleaned up repo for spring

---
+++ in_class_notes/ch2_getting_started/1_15_notes.cpp
@@ -0,0 +1,30 @@
+/*
+Matt Welz
+Class Practice
+1/15/2025
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world!" << endl << endl << endl;  //our first cout statement
+	cout << "Print more stuff" << endl;
+
+	//declares three integers x,y,z
+	int x;
+	int y;
+	int z;
+	x = 17;
+	y = 59;
+	z = 97;
+	cout << "The sum of my three variables: " << x + y + z << endl;
+	x = 2873289;
+	cout << "The new value of x is " << x << endl;
+	y = y + z;
+
+
+	return 0;
+}
