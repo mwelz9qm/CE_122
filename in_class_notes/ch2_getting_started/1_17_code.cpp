8e0ee0743bac4893d3c2e48cbeb503327ad8c6c5
Matt Welz <matthew.d.welz@gmail.com>
2025-01-20 09:34:25 -0700

    added 1_17 class code

---
+++ in_class_notes/ch2_getting_started/1_17_code.cpp
@@ -0,0 +1,28 @@
+/*
+Class 1/17/2025
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Practicing printing! " << endl;
+
+	int a;
+	a = 5;
+	cout << " Value of a: " << a << endl;
+	a = 5 * 10 + a;
+	cout << " Value of a: " << a << endl;
+	cout << " The value of 17/7 is " << 17.0 / 7 << endl;
+
+	double x;
+	x = 2763.282739;
+
+	cout << " 23768/132 =  " << 23768 / 132 << endl;
+	cout << " Remainder of 23768/132 =  " << 23768 % 132 << endl;
+
+
+	return 0;
+}
